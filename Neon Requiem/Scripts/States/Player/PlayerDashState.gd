d96a4052cbd7ab8f16510119a768ebe7c74f07dc
Sebastian Lopez Melendrez <90015639+lopezmseb@users.noreply.github.com>
2024-11-04 17:36:20 -0600

    Player: Dash Cooldown Implemented

--- Neon Requiem/Scripts/States/Player/PlayerDashState.gd
+++ Neon Requiem/Scripts/States/Player/PlayerDashState.gd
@@ -4,6 +4,7 @@ var dashVelocity: Vector2
 const dashSpeed: float = 10
 
 @export var dashTimer : Timer
+@export var cooldownTimer: Timer
 
 func Enter():
 	dashVelocity = player.velocity
@@ -11,6 +12,7 @@ func Enter():
 
 func Physics_Update(delta):
 	if(dashTimer.is_stopped()):
+		cooldownTimer.start()
 		onNewState.emit(self, AvailableStates.Movement)
 	
 	player.velocity = dashVelocity * dashSpeed
--- Neon Requiem/Scripts/States/Player/PlayerMoveState.gd
+++ Neon Requiem/Scripts/States/Player/PlayerMoveState.gd
@@ -1,7 +1,9 @@
 extends PlayerState
 
+@export var dashCooldown: Timer
+
 func Physics_Update(delta):
-	if(Input.is_action_just_pressed("dash")):
+	if(Input.is_action_just_pressed("dash") && dashCooldown.is_stopped()):
 		onNewState.emit(self, AvailableStates.Dash)
 		
 	var moveDirection = Input.get_vector("left","right","up","down");

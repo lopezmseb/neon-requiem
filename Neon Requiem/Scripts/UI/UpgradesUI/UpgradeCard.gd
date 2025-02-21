a9591952f5f84b6df080ba98cafab869af78d8f0
Sebastian Lopez <90015639+lopezmseb@users.noreply.github.com>
2025-02-20 19:38:14 -0600

    change upgrade level to text

--- Neon Requiem/Scripts/UI/UpgradesUI/UpgradeCard.gd
+++ Neon Requiem/Scripts/UI/UpgradesUI/UpgradeCard.gd
@@ -4,6 +4,7 @@ class_name UpgradeCard
 @export var upgradeStrategy: UpgradeStrategy = null
 @onready var description: RichTextLabel = $ColorRect/Button/VBoxContainer/Description
 @onready var title: RichTextLabel = $ColorRect/Button/VBoxContainer/UpgradeTitle
+@onready var levelText = $ColorRect/Button/VBoxContainer/LevelAmount
 
 signal onClick
 
@@ -12,6 +13,11 @@ func _ready():
 	if(upgradeStrategy):
 		description.text = "[center]{description}".format({"description": upgradeStrategy.upgradeText})
 		title.text = "[center]{title}".format({"title":upgradeStrategy.upgradeTitle})
+		if(upgradeStrategy.level > 0):
+			levelText.text = "[center]Level {level}".format({"level": upgradeStrategy.level})
+		else:
+			levelText.text = "[center]Not Acquired"
+			
 
 func _process(delta):
 	size_flags_horizontal = Control.SIZE_EXPAND_FILL

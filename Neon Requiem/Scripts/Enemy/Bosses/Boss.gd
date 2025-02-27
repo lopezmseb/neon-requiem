28c9bec07335eb16d9bb1f2edd8b119caa14965f
Sebastian Lopez Melendrez <90015639+lopezmseb@users.noreply.github.com>
2025-02-27 17:36:32 -0600

    Added Attacks

--- Neon Requiem/Scripts/Enemy/Bosses/Boss.gd
+++ Neon Requiem/Scripts/Enemy/Bosses/Boss.gd
@@ -1,4 +1,4 @@
 extends CharacterBody2D
 
 func _physics_process(delta):
-	move_and_slide()
+	pass
--- Neon Requiem/Scripts/States/Bosses/BossAttack.gd
+++ Neon Requiem/Scripts/States/Bosses/BossAttack.gd
@@ -9,20 +9,25 @@ const bulletSpeed = 500.0
 
 func Enter():
 	rng = RandomNumberGenerator.new()
-	animate.play("Attack")
+	animate.play("Idle")
 
 func Physics_Update(delta: float):
+	var attackToChoose = rng.randi_range(0,1)
 	if(readyToAttack):
-		Attack_All_Around()
-		readyToAttack = false
+		Spread_Attack()
+#		match attackToChoose:
+#			0:				
+#				Attack_All_Around()
+#			1: 				
+#				AAA_Burst()
 
-func Attack_All_Around():
+func Attack_All_Around(offset:float = 0.0):
 	var degrees = 360
 	var bulletAmount = 12
 	var degreeIncrease = degrees/bulletAmount
 	
 	for i in range(0, bulletAmount):
-		var radian = deg_to_rad(degreeIncrease * i)
+		var radian = deg_to_rad((degreeIncrease * i) + offset)
 		var newPos = enemy.position + Vector2(cos(radian), sin(radian)) * 50
 		gun.look_at(newPos)
 		
@@ -36,16 +41,50 @@ func Attack_All_Around():
 		bullet.set_collision_layer_value(2, false)
 		bullet.set_collision_mask_value(2, false)
 		
-		print(bullet.get_collision_layer())
-		print(bullet.get_collision_mask())
+		# Set the bullet's velocity and rotation based on the direction to the mouse.\
+		var direction = (newPos - enemy.position).normalized()
+		bullet.velocity = direction * bulletSpeed
+		bullet.rotation = direction.angle()
+	
+	readyToAttack = false
+	
+func AAA_Burst():
+	for i in range(0,3):
+		$BurstTimer.start()
+		Attack_All_Around(0 if i%2 == 1 else 15)
+		await $BurstTimer.timeout
+		
+func Spread_Attack():
+	var degrees = 360
+	var bulletAmount = 12
+	var degreeIncrease = degrees/bulletAmount
+	var offset = 0
+	
+	for i in range(0, bulletAmount):
+		var radian = deg_to_rad((45 + 90 * i) + offset)
+		var newPos = enemy.position + Vector2(cos(radian), sin(radian)) * 50
+		gun.look_at(newPos)
+		
+		$AttackCooldown.start()
+		var bullet = bulletPath.instantiate()
+		get_tree().root.add_child(bullet)
+
+		# Position the bullet at the player's shooting point (Marker2D).
+		bullet.position = newPos
+		bullet.source = "Enemy"
+		bullet.set_collision_layer_value(2, false)
+		bullet.set_collision_mask_value(2, false)
 		
 		# Set the bullet's velocity and rotation based on the direction to the mouse.\
 		var direction = (newPos - enemy.position).normalized()
 		bullet.velocity = direction * bulletSpeed
 		bullet.rotation = direction.angle()
 	
-
+	readyToAttack = false
+	
+		
 
 
 func _on_attack_cooldown_timeout():
 	readyToAttack = true
+	

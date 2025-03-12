4606d9cd2a4edf046dcba3b252db2c57421e9146
Nathan Cameron <62361875+cameronnathan707@users.noreply.github.com>
2025-03-12 13:32:00 -0600

    change value

--- Neon Requiem/Scripts/Upgrades/HealthPack.gd
+++ Neon Requiem/Scripts/Upgrades/HealthPack.gd
@@ -4,8 +4,8 @@ class_name HealthPack
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		if body.healthComponent.currentHealth != body.healthComponent.MAX_HEALTH:
-			if body.healthComponent.currentHealth + 10 < body.healthComponent.MAX_HEALTH:
-				body.healthComponent.currentHealth = body.healthComponent.currentHealth + 10
+			if body.healthComponent.currentHealth + 20 < body.healthComponent.MAX_HEALTH:
+				body.healthComponent.currentHealth = body.healthComponent.currentHealth + 20
 				queue_free()
 			else: 
 				body.healthComponent.currentHealth = body.healthComponent.MAX_HEALTH
